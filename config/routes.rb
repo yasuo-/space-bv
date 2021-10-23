76b3b9161d3ae7adf7f07df6dcb7313f3366e990
yasuo kurihara <breitling1216@gmail.com>
2021-10-23 15:04:10 +0900

    add user
    devise base

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  # root
   root 'static_pages#home'
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+
   get 'static_pages/home'
   get '/help', to: 'static_pages#help'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
